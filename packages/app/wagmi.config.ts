fd4f61286e7531bf95be37ca61bb8389ed5f2b18
Zarilex09 <97900858+Zarilex09@users.noreply.github.com>
2024-04-29 22:52:55 +0200

    Update wagmi.config.ts

--- packages/app/wagmi.config.ts
+++ packages/app/wagmi.config.ts
@@ -13,14 +13,6 @@ export default defineConfig({
           11155111: '0xF11f180eE37dd6aa7dD08b8C1Cd670fC4DBE0e34',
         },
       },
-    }),
-    foundry({
-      project: '../foundry',
-      deployments: {
-        Message: {
-          11155111: '0xcc5a0d6268d70811edad77799f2168afe6382e89',
-        },
-      },
-    }),
+    })
   ],
 })
