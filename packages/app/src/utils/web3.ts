82a9266707c9e423dd5cd360cf15add9eda0247b
Zarilex09 <97900858+Zarilex09@users.noreply.github.com>
2024-05-01 15:36:43 +0200

    Update web3.ts

--- packages/app/src/utils/web3.ts
+++ packages/app/src/utils/web3.ts
@@ -9,7 +9,7 @@ if (!WALLETCONNECT_PROJECT_ID) {
 }
 
 export const WALLETCONNECT_CONFIG = defaultWagmiConfig({
-  projectId: '55d75c54c9473dfc84038c7922ac2d9a',
+  projectId: WALLETCONNECT_PROJECT_ID,
   chains: ETH_CHAINS,
   metadata: {
     name: SITE_NAME,
