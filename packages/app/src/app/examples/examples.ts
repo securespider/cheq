3df49023ab7ad0f18bb2d7c293ee730b35a9bf15
ryano245 <e0726366@u.nus.edu>
2024-05-13 14:10:43 +0200

    final edits

--- packages/app/src/app/examples/examples.ts
+++ packages/app/src/app/examples/examples.ts
@@ -3,12 +3,7 @@ import NotificationIcon from '@/assets/icons/notification.png'
 import TokenIcon from '@/assets/icons/token.png'
 
 export const EXAMPLE_ITEMS = [
-  {
-    title: 'testing',
-    description: 'Testing code to learn - Ryan',
-    image: EtherIcon.src,
-    url: '/examples/testing_api'
-  },
+
   {
     title:"Mint NFT",
     description: 'This is a text page to mint NFT',
--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react'; // Import useEffect hook
 import { CardList } from '@/components/CardList';
-import { SITE_DESCRIPTION, SITE_NAME } from '@/utils/site';
+// import { SITE_DESCRIPTION, SITE_NAME } from '@/utils/site';
 import { EXAMPLE_ITEMS } from './examples/examples';
 
 export default function Home() {
 
   return (
     <>
-      <h2 className='text-2xl mb-2'>{SITE_NAME}</h2>
-      <p>{SITE_DESCRIPTION}</p>
+      {/* <h2 className='text-2xl mb-2'>{SITE_NAME}</h2>
+      <p>{SITE_DESCRIPTION}</p> */}
 
       {/* Examples section */}
       <div className='mt-4'>
-        <h3 className='text-lg mb-2'>Examples</h3>
+        <h2 className='text-lg mb-2'>CHEQ</h2>
         <p className='mb-4'>
-          The following examples are used for demo purposes and help you bootstrap development. You can find the example
-          the main repo at <code>src/app/examples</code>. Feel free to delete this section and the examples folder for
-          your own App.
+          This is the home page of our prototype application 'CHEQ'. Here you are able to mint NFT for verified items by clicking the mint NFT button below. 
+          You are also able to check out the items that you currently own by clicking on the 'Profile' tab at the top of the home page.
         </p>
 
         <CardList items={EXAMPLE_ITEMS} />
@@ -24,5 +23,3 @@ export default function Home() {
     </>
   );
 }
-
-
